135f9cb9d96cf473fc20019b9c57c436ce0f1853
dBuicke <74936367+dBuicke@users.noreply.github.com>
2020-12-15 13:50:24 +0000

    First Commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    [SerializeField] private float movementSpeed = 5f;
+    [SerializeField] private float rotationSpeed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        rb.rotation -= Input.GetAxis("Horizontal") * rotationSpeed;
+        rb.velocity = transform.up * Input.GetAxis("Vertical") * movementSpeed;
+    }
+}
